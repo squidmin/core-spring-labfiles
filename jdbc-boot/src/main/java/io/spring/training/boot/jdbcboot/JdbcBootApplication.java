1faa9fa0d3b4050b324aecab79987a0d9bba5b02
squidmin <morse.james.r@gmail.com>
2024-07-31 21:42:37 -0500

    Create 'jdbc-boot' application

---
+++ jdbc-boot/src/main/java/io/spring/training/boot/jdbcboot/JdbcBootApplication.java
@@ -0,0 +1,13 @@
+package io.spring.training.boot.jdbcboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcBootApplication.class, args);
+    }
+
+}
---
+++ jdbc-boot/src/test/java/io/spring/training/boot/jdbcboot/JdbcBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.training.boot.jdbcboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
